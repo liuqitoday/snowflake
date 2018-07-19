f990d64f16d30e8b0d954b02ca51fadaca1d3ad0
liuqi <liuqitoday@163.com>
2018-07-19 18:04:23 +0800

    项目初始化

---
+++ src/main/java/com/liuqitech/snowflake/SnowflakeApplication.java
@@ -0,0 +1,12 @@
+package com.liuqitech.snowflake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowflakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowflakeApplication.class, args);
+    }
+}
